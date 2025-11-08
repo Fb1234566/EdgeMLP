e26adb5570235154fa4a0c39fed17f7dfeac5732
Filippo Benedetti <filippo.benedetti3@gmail.com>
2025-11-08 16:35:33 +0100

    Added basic matrix functionalities

---
+++ src/training/include/Matrix.h
@@ -0,0 +1,31 @@
+#ifndef EDGEMLP_MATRIX_H
+#define EDGEMLP_MATRIX_H
+
+#include <iostream>
+#include <vector>
+
+class Matrix
+{
+private:
+    int rows;
+    int cols;
+    std::vector<double> data;
+public:
+    Matrix(int rows, int cols);
+    ~Matrix();
+    double& operator()(int row, int col);
+    double operator()(int row, int col) const;
+
+    int getRows() const;
+    int getCols() const;
+    const double* getData() const;
+    Matrix operator*(const Matrix& other);
+    Matrix operator+(const Matrix& other);
+    Matrix transpose();
+    Matrix hadamardProduct(const Matrix& other);
+    Matrix operator*(double scalar);
+    Matrix operator+(double scalar);
+};
+
+std::ostream& operator<<(std::ostream& os, Matrix& matrix);
+#endif //EDGEMLP_MATRIX_H
---
+++ src/training/src/Matrix.cpp
@@ -0,0 +1,160 @@
+#include "Matrix.h"
+
+#include <stdexcept>
+
+Matrix::Matrix(const int rows, const int cols) : rows(rows), cols(cols), data(rows * cols, 0.0)
+{
+}
+
+int Matrix::getRows() const
+{
+    return rows;
+}
+
+int Matrix::getCols() const
+{
+    return cols;
+}
+
+double& Matrix::operator()(const int row, const int col)
+{
+    return data[row * cols + col];
+}
+
+double Matrix::operator()(const int row, const int col) const
+{
+    return data[row * cols + col];
+}
+
+const double* Matrix::getData() const
+{
+    return data.data();
+}
+
+Matrix::~Matrix()
+{
+}
+
+Matrix Matrix::operator*(const Matrix& other)
+{
+    if (cols != other.rows)
+    {
+        throw std::invalid_argument(
+            "Cannot multiply matrices with incompatible dimensions " + std::to_string(cols) + " and " +
+            std::to_string(other.rows));
+    }
+
+    Matrix result(rows, other.cols);
+#pragma omp parallel for collapse(2)
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < other.cols; j++)
+        {
+            double sum = 0;
+            for (int k = 0; k < cols; k++)
+            {
+                sum += (*this)(i, k) * other(k, j);
+            }
+            result(i, j) = sum;
+        }
+    }
+    return result;
+}
+
+Matrix Matrix::operator+(const Matrix& other)
+{
+    if (rows != other.rows || cols != other.cols)
+    {
+        throw std::invalid_argument(
+            "Cannot add matrices with incompatible dimensions" + std::to_string(cols) + " and " + std::to_string(
+                other.rows));
+    }
+
+    Matrix result(rows, cols);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            result(i, j) = (*this)(i, j) + other(i, j);
+        }
+    }
+
+    return result;
+}
+
+Matrix Matrix::transpose()
+{
+    Matrix result(cols, rows);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            result(j, i) = (*this)(i, j);
+        }
+    }
+
+    return result;
+}
+
+Matrix Matrix::hadamardProduct(const Matrix& other)
+{
+    Matrix result(rows, other.cols);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < other.cols; j++)
+        {
+            result(i, j) = (*this)(i, j) * other(i, j);
+        }
+    }
+
+    return result;
+}
+
+Matrix Matrix::operator*(const double scalar)
+{
+    Matrix result(rows, cols);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            result(i, j) = (*this)(i, j) * scalar;
+        }
+    }
+    return result;
+}
+
+Matrix Matrix::operator+(const double scalar)
+{
+    Matrix result(rows, cols);
+    for (int i = 0; i < rows; i++ )
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            result(i, j) = (*this)(i, j) + scalar;
+        }
+    }
+    return result;
+}
+
+
+std::ostream& operator<<(std::ostream& os, Matrix& matrix)
+{
+    os << "Matrix [" << matrix.getRows() << "x" << matrix.getCols() << "]:\n";
+    for (int i = 0; i < matrix.getRows(); ++i)
+    {
+        os << "[ ";
+        for (int j = 0; j < matrix.getCols(); ++j)
+        {
+            os << matrix(i, j);
+            if (j < matrix.getRows() - 1)
+                os << ", ";
+        }
+        os << " ]\n";
+    }
+    os << std::endl;
+    return os;
+}
